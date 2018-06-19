455541d462f18cfccc9c2fe146d395249ab45722
cuisongliu <cuisongliu@qq.com>
2018-06-20 00:12:31 +0800

    leetcode 代码

--- src/data/structures/stack/Main.java
+++ src/data/structures/stack/Main.java
@@ -39,4 +39,29 @@ public class Main {
         stack.pop();
         System.out.println(stack);
     }
+
+    public static Boolean isValid(String s){
+        Stack<Character> stack = new ArrayStack<>();
+        for (int i = 0; i < s.length();i++){
+            char c = s.charAt(i);
+            if (c =='[' || c=='(' || c=='{') {
+                stack.push(c);
+            }else {
+                if (stack.isEmpty()) return false;
+                else {
+                    char top = stack.pop();
+                    if (top != ')' && c=='(') {
+                        return false;
+                    }
+                    if (top != ']' && c=='[') {
+                        return false;
+                    }
+                    if (top != '}' && c=='{') {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
 }
