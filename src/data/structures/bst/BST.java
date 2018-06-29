96d1adaef8bf1e0fcd650ffc0dc6d06b9f1f362e
cuisongliu <cuisongliu@qq.com>
2018-06-29 16:10:13 +0800

    bst 二分搜索树  前中后序遍历

--- src/data/structures/bst/BST.java
+++ src/data/structures/bst/BST.java
@@ -75,4 +75,62 @@ public class BST<E extends Comparable<E>> {
         }
         return  node;
     }
+
+    //看二分搜索树上是否包含元素
+    public boolean contains(E e){
+        return contains(root,e);
+    }
+
+    private boolean contains(Node node, E e) {
+        if (node == null) {
+            return false;
+        } else {
+            if (node.e.compareTo(e) > 0) {
+                return contains(node.left, e);
+            } else if (node.e.compareTo(e) < 0) {
+                return contains(node.right, e);
+            }else {
+                return true;
+            }
+        }
+    }
+
+    //前序遍历 最自然的遍历方式  root -> 左子树 -> 右子树
+    public void preOrder(){
+        preOrder(root);
+    }
+
+    private void preOrder(Node node){
+        if (node!=null){
+            System.out.print(node.e+"->");
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+    }
+    //中序遍历 排序后 排序树  左子树 -> root -> 右子树
+    public void order(){
+        order(root);
+    }
+
+    private void order(Node node){
+        if (node!=null){
+            order(node.left);
+            System.out.print(node.e+"->");
+            order(node.right);
+        }
+    }
+
+    //后序遍历     左子树 -> 右子树 -> root
+    public void sufOrder(){
+        suffOrder(root);
+    }
+
+    private void suffOrder(Node node){
+        if (node!=null){
+            suffOrder(node.left);
+            suffOrder(node.right);
+            System.out.print(node.e+"->");
+        }
+    }
+
 }
